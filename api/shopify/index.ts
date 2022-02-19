16e41e241b35b0927182003acbf8099cefc5619d
wint1703 <isuszanty@gmail.com>
2022-02-19 18:56:13 +0200

    added shopify client

---
+++ api/shopify/index.ts
@@ -0,0 +1,5 @@
+import Shopify from "@shopify/shopify-api";
+
+const client = new Shopify.Clients.Graphql(process.env.SHOPIFY_DOMAIN!, process.env.SHOPIFY_ACCESS_TOKEN!);
+
+export default client
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SHOPIFY_DOMAIN: "wint1703.myshopify.com",
+    SHOPIFY_ACCESS_TOKEN: "a03d293226e8b0e343c66d88ac669f44",
+  }
 }
 
 module.exports = nextConfig
