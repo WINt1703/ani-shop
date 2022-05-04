8c9110585a5888746db788c9490b73bc434fc0ca
wint1703 <isuszanty@gmail.com>
2022-05-04 20:48:24 +0300

    refactored env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    SHOPIFY_DOMAIN: "hydrogen-preview.myshopify.com",
-    SHOPIFY_ACCESS_TOKEN: "3b580e70970c4528da70c98e097c2fa0",
-  },
   images: {
     domains: ['cdn.shopify.com'],
   },
