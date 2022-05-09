33c849c519807260af3b8cc2d55b2ba0202aedd9
wint1703 <isuszanty@gmail.com>
2022-05-10 01:45:00 +0300

    changed config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.shopify.com'],
   },
+  assetPrefix: isProd ? "/ani-shop/" : "",
 }
 
 module.exports = nextConfig
