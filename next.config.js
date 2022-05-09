8400552f97fb71b6888f8fcf6bde715a70c464bb
wint1703 <isuszanty@gmail.com>
2022-05-10 02:23:26 +0300

    added image loader

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    loader: isProd ? 'cloudinary' : undefined,
+    path: isProd ? "https://wint1703.github.io/ani-shop/" : undefined,
     domains: ['cdn.shopify.com'],
   },
   assetPrefix: isProd ? "/ani-shop/" : "",
